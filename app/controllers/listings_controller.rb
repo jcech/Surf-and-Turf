d3646222f2dd4f246c3bf8e45e7c30b3addabbe1
John Cech & Drew Seminara <student@epicodus.com>
2014-04-16 11:05:51 -0700

    add login functionality to site

---
+++ app/controllers/listings_controller.rb
@@ -0,0 +1,26 @@
+class ListingsController < ApplicationController
+
+  def index
+    @listings = Listing.all
+  end
+
+  def new
+    @listing = Listing.new
+  end
+
+  def create
+    @listing = Listing.new(listing_params)
+    if @listing.save
+      flash[:notice] = "Listing has been posted!"
+      redirect_to root_url
+    else
+      render 'new'
+    end
+  end
+
+private
+  def listing_params
+    params.require(:listing).permit(:name, :body, :user_id)
+  end
+
+end
